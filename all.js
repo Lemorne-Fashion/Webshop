061f8da171e3239fc6a4e180b96147071206d661
NoahVP <37066731+NoahVP@users.noreply.github.com>
2020-04-30 18:13:16 +0200

    hihi

---
+++ all.js
@@ -0,0 +1,41 @@
+const itemlist = document.getElementById('itemlist');
+const searchBar = document.getElementById('searchBar');
+let allitems = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    console.log(searchString);
+    const filteredItems = allitems.filter((Item) => {
+        return (
+            Item.name.toLowerCase().includes(searchString)
+        );
+    });
+    displayItems(filteredItems);
+});
+
+const loadItems = async () => {
+    try {
+        const res = await fetch('https://raw.githubusercontent.com/Lemorne-Fashion/Webshop/master/api.txt');
+        allitems = await res.json();
+        displayItems(allitems);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const displayItems = (Items) => {
+    const htmlString = Items
+        .map((Item) => {
+            return `
+            <li class="Item">
+                <h2>${Item.name}</h2>
+                <p>gender: ${Item.gender}</p>
+                <img src="${Item.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    itemlist.innerHTML = htmlString;
+};
+
+loadItems();

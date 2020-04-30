ad3bf30930becc664ceabb1f55639e96c01c74e4
NoahVP <37066731+NoahVP@users.noreply.github.com>
2020-05-01 00:11:30 +0200

    Update woman.js

--- woman.js
+++ woman.js
@@ -14,7 +14,7 @@ searchBar.addEventListener('keyup', (e) => {
 });
 
 const loadItems = async () => {
-        const res = await fetch('https://raw.githubusercontent.com/Lemorne-Fashion/Webshop/master/womanapi.txt');
+        const res = await fetch('https://raw.githubusercontent.com/Lemorne-Fashion/Webshop/master/apiwoman.txt');
         allitems = await res.json();
         displayItems(allitems);
 };

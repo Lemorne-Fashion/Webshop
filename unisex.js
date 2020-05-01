cff4933afb0d2b8a01dac5716531e6ba1e6c1b75
NoahVP <37066731+NoahVP@users.noreply.github.com>
2020-05-01 13:52:21 +0200

    a

---
+++ unisex.js
@@ -0,0 +1,37 @@
+const itemlist = document.getElementById('itemlist');
+const searchBar = document.getElementById('searchBar');
+let allitems = [];
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    console.log(searchString);
+    const filteredItems = allitems.filter((Item) => {
+        return (
+            Item.name.toLowerCase().includes(searchString)
+        );
+    });
+    displayItems(filteredItems);
+});
+
+const loadItems = async () => {
+        const res = await fetch('https://raw.githubusercontent.com/Lemorne-Fashion/Webshop/master/api.txt');
+        allitems = await res.json();
+        displayItems(allitems);
+};
+
+const displayItems = (Items) => {
+    const htmlString = Items
+        .map((Item) => {
+            return `
+            <li class="Item">
+                <h2>${Item.name}</h2>
+                <p>Price: ${Item.price}</p>
+                <img src="${Item.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    itemlist.innerHTML = htmlString;
+};
+
+loadItems();
